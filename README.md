b3cb293fef2149dc2730079b532e503d97062c05
Johan Andersson <ja222um@student.lnu.se>
2020-09-15 17:01:50 +0200

    Added tool script. First commit, as well.

---
+++ README.md
@@ -0,0 +1,4 @@
+# bash-scripts
+Just some various bash-scripts I create to make everyday work easier.
+
+- install-tools-script-sh // temporary(?) simple script to just install some tools I need.
---
+++ install-tools-script.sh
@@ -0,0 +1,86 @@
+echo "---Brocahontaz installation script---\n"
+echo "Installing the tools I need :-)\n"
+echo "-------------------------------------\n"
+
+# Update device
+echo "Updating repositories and upgrading"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# Install htop
+echo "Installing htop"
+sudo apt install htop -y
+
+# Intall git
+echo "Installing git"
+sudo apt-get install git -y
+
+# Install zsh
+echo "Installing zsh"
+sudo apt install zsh -y
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k
+echo "Installing powerlevel10k zsh theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install nvm
+echo "Installing nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Install latest node
+echo "Using nvm to install latest node version"
+nvm install node
+
+# Install vscode
+echo "Installing vscode"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y
+
+# Install Docker
+echo "Installing Docker"
+echo "Setting up repository"
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+echo "installing Docker Engine"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Install Docker-Compose
+echo "Installing Docker-Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)" -o ~/bin/docker-compose
+sudo chmod +x ~/bin/docker-compose
+
+# Install Slack
+echo "Installing Slack"
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.8.0-amd64.deb
+sudo apt install ./slack-desktop-*.deb
+
+# Install Discord
+echo "Installing Discord"
+sudo snap install discord
+
+# Install Ansible
+echo "Installing Ansible"
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+# Clean up device
+echo "Post-install cleanup"
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
